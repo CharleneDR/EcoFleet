dfbe54e4dbcf690cd9042fb73ad4d9aa816f7196
Grom69 <jerome.dannfald@orange.fr>
2023-01-12 13:54:40 +0100

    login correction, it works again !

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/', name: 'app_login')]
+    #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
