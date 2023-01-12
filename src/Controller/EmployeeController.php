7a51044ae28b49457398925e80a7ad301bb460c0
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-01-12 15:30:45 +0100

    work in progress

---
+++ src/Controller/EmployeeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmployeeController extends AbstractController
+{
+    #[Route('/employee', name: 'app_employee')]
+    public function index(): Response
+    {
+        return $this->render('employee/index.html.twig', [
+            'controller_name' => 'EmployeeController',
+        ]);
+    }
+}
